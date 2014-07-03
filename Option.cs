a40b86335b30f9163d96733bd557ea311705feea
Stuart Haidon <shaidon@serayen.com>
2014-07-03 22:40:02 +0100

    Added DateOption, IntListOption, and StringOption types.

--- Option.cs
+++ Option.cs
@@ -9,10 +9,14 @@ namespace PdoxParserTest
 	enum OptionType
 	{
 		Group,
-		String,
+		String,	 
+		StringList,
 		Bool,
 		Float,
-		Integer
+		Integer,
+		IntList,
+		Colour,
+		Date,
 	}
 
 	[DebuggerDisplay( "Tag: {GetIDString}, Data: {GetValueString}" )]
@@ -37,14 +41,20 @@ namespace PdoxParserTest
 		public static Option Parse( string tag, string data )
 		{
 			Option opt;
-			
-			if ( Char.IsDigit( data[0] ) || (data[0] == '-' && Char.IsDigit( data[1] )) )
+
+			if( Char.IsDigit( data[0] ) || ( data[0] == '-' && Char.IsDigit( data[1] ) ) )
 			{
-				if ( data.Contains( "." ) )
-					opt = new FloatOption( tag, Double.Parse( data, CultureInfo.InvariantCulture ) );
-				else
+				if( data.Contains( "." ) )
+				{
+					if( data.IndexOf( "." ) != data.LastIndexOf( "." ) )
+					{
+						string[] parts = data.Split( '.' );
+						opt = new DateOption( tag, new DateTime( Int32.Parse( parts[0] ), Int32.Parse( parts[1] ), Int32.Parse( parts[1] ) ) );
+					} else
+						opt = new FloatOption( tag, Double.Parse( data, CultureInfo.InvariantCulture ) );
+				} else
 					opt = new IntegerOption( tag, Int32.Parse( data ) );
-			} else if ( data == "yes" || data == "no" )
+			} else if( data == "yes" || data == "no" )
 			{
 				opt = new BoolOption( tag, data == "yes" );
 			} else
@@ -238,11 +248,11 @@ namespace PdoxParserTest
 		private readonly string m_id;
 		private readonly Color m_value;
 
-		public ColourOption( string id, Color rgb )
-			: base( OptionType.Float )
+		public ColourOption( string id, Color value )
+			: base( OptionType.Colour )
 		{
 			m_id = id;
-			m_value = rgb;
+			m_value = value;
 		}
 
 		public Color Value
@@ -266,6 +276,104 @@ namespace PdoxParserTest
 			get
 			{
 				return m_value.ToString();
+			}
+		}
+	}
+
+	[DebuggerDisplay( "Tag: {m_id}, Data: {m_value}" )]
+	class DateOption : Option
+	{
+		private readonly string m_id;
+		private readonly DateTime m_value;
+
+		public DateOption( string id, DateTime value )
+			: base( OptionType.Date )
+		{
+			m_id = id;
+			m_value = value;
+		}
+
+		public DateTime Value
+		{
+			get
+			{
+				return m_value;
+			}
+		}
+
+		public override string GetIDString
+		{
+			get
+			{
+				return m_id;
+			}
+		}
+
+		public override string GetValueString
+		{
+			get
+			{
+				return m_value.ToString( CultureInfo.InvariantCulture );
+			}
+		}
+	}
+
+	[DebuggerDisplay( "Tag: {m_id}, Data Count: {Items.Count}" )]
+	class IntListOption : Option
+	{
+		private readonly string m_id;
+		public readonly IList<int> Items;
+
+		public IntListOption( string id, IList<int> value )
+			: base( OptionType.IntList )
+		{
+			m_id = id;
+			Items = value;
+		}
+
+		public override string GetIDString
+		{
+			get
+			{
+				return m_id;
+			}
+		}
+
+		public override string GetValueString
+		{
+			get
+			{
+				return Items.Count.ToString();
+			}
+		}
+	}
+
+	[DebuggerDisplay( "Tag: {m_id}, Data Count: {Items.Count}" )]
+	class StringListOption : Option
+	{
+		private readonly string m_id;
+		public readonly IList<string> Items;
+
+		public StringListOption( string id, IList<string> value )
+			: base( OptionType.StringList )
+		{
+			m_id = id;
+			Items = value;
+		}
+
+		public override string GetIDString
+		{
+			get
+			{
+				return m_id;
+			}
+		}
+
+		public override string GetValueString
+		{
+			get
+			{
+				return Items.Count.ToString();
 			}
 		}
 	}
